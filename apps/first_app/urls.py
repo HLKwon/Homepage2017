05fe25752b39080a4c79db8205b1142117a6ee2a
Louis Kwon <hanlerris@gmail.com>
2017-04-04 02:14:13 -0400

    Second Update

--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^about$', views.about),
+    url(r'^projects$', views.projects),
+    url(r'^contact$', views.contact),
     url(r'^test$', views.test),
 ]
--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
+# from collection.forms import ContactForm
 import re
 
 # Create your views here.
 def index(request):
     return render(request, "first_app/index.html")
+
+def about(request):
+    return render(request, "first_app/about.html")
+
+def projects(request):
+    return render(request, "first_app/projects.html")
+
+def contact(request):
+    # form_class = ContactForm
+
+    return render(request, 'first_app/contact.html', {
+        # 'form': form_class,
+    })
+
+
 
 def test(request):
     response = "Hello I am your first request!"
