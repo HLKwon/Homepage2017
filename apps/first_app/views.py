a96f9b2d00fa3cb52f542b53e059b08bae80f266
Louis Kwon <hanlerris@gmail.com>
2017-04-09 00:05:12 -0400

    Contact form added

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
-# from collection.forms import ContactForm
+from .forms import ContactForm
 import re
+from django.core.mail import EmailMessage
+from django.template import Context
+from django.template.loader import get_template
+
+
 
 # Create your views here.
 def index(request):
@@ -14,11 +19,41 @@ def projects(request):
     return render(request, "first_app/projects.html")
 
 def contact(request):
-    # form_class = ContactForm
+    form_class = ContactForm
 
-    return render(request, 'first_app/contact.html', {
-        # 'form': form_class,
-    })
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the
+            # contact information
+            template = get_template('first_app/contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                contact_email,
+                # "Your website" +'',
+                ['louis@nuri.codes'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('contact')
+
+
+    return render(request, 'first_app/contact.html', {'form': form_class})
+
+
 
 
 
