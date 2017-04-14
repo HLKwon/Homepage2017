9f280deeee61ec1ed6513576da6f706920c7115a
Louis Kwon <hanlerris@gmail.com>
2017-04-13 23:48:03 -0400

    removed static folder. Reverted the git.

--- homepage/settings.py
+++ homepage/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#3kkcgkn-ln&#!^n&4x0318$^*521mx5*^v5)#mlag0jof0yv-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'54.165.117.58',
+	'.nuri.codes',
+]
 
 
 # Application definition
@@ -121,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Email setup for Contact Form
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
